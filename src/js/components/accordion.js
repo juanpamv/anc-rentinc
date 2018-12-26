249441c90cba07c15765ba57240614305163a6f7
Unknown <manfred19@gmail.com>
2018-12-26 08:44:47 -0600

    add professional section functionality

---
+++ src/js/components/accordion.js
@@ -0,0 +1,49 @@
+// Module Reveal Design Pattern
+// References
+// https://www.udemy.com/javascript-design-patterns-20-patterns-for-expert-code
+// https://medium.com/@jrschwane/writing-modular-javascript-pt-1-b42a3bd23685
+
+// TEMPLATE
+// Component
+
+var accordion = (function() {
+    "use strict";
+
+    // Variable Definitions - Public Variables
+    var DOM = {
+        plusSign: $('.accordion-container'),
+        plusSignText: $('.accordion-container .toggle-accordion')
+    };
+
+    /* =================== Public methods ================= */
+
+    
+    function toggleIcon() {
+        $(this).toggleClass('opened');
+        $(this).find(".toggle-accordion").text($(this).text() == 'Ver más' ? 'Ver menos' : 'Ver más');
+    }
+
+    // cache DOM elements
+    function cacheDom() {
+        DOM.plusSign = $('.accordion-container');
+        DOM.plusSignText = $('.accordion-container .toggle-accordion');
+    }
+ 
+    // Bind Events To UI
+    function bindEvents() {
+        DOM.plusSign.click(toggleIcon);
+    }
+
+    // Component Init
+    function init() {
+        cacheDom();
+        bindEvents();
+    }
+
+    /* =============== export public methods =============== */
+    return {
+        init: init
+    };
+})();
+
+export { accordion };
